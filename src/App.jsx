9835f0c22a4258c29a88e6bd6c888c685d9196b7
Parth Movaliya <parthmovaliya55055@gmail.com>
2023-02-20 18:36:18 +0530

    setup login-signup html

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Home from './home/Home'
+import Login from './login/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+      <Login />
+    </div>
   )
 }
 
